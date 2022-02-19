15894ebe2424885ee179f81ef5f60277280bf7e7
Chris M <821688+tebriel@users.noreply.github.com>
2022-02-19 16:37:19 +0000

    Secrets

--- infrastructure/fry-bot.tf
+++ infrastructure/fry-bot.tf
@@ -25,6 +25,7 @@ resource "azurerm_container_group" "fry-bot" {
     secure_environment_variables = {
       BOT_GATEWAY_TOKEN               = var.BOT_GATEWAY_TOKEN
       AZURE_STORAGE_CONNECTION_STRING = azurerm_storage_account.fry-bot.primary_connection_string
+      STORAGE_BASE_URL                = azurerm_storage_account.fry-bot.primary_blob_endpoint
     }
 
     ports {
--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -3,3 +3,9 @@ resource "github_actions_secret" "storage-url" {
   secret_name     = "STORAGE_BASE_URL"
   plaintext_value = azurerm_storage_account.fry-bot.primary_table_host
 }
+
+resource "github_actions_secret" "registry-url" {
+  repository      = "fry-bot"
+  secret_name     = "REGISTRY_URL"
+  plaintext_value = "gchr.io"
+}
