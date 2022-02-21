c9a7600f941054b0c55948f5812e6ab16bddad2c
Chris M <821688+tebriel@users.noreply.github.com>
2022-02-21 18:13:15 +0000

    init terraform

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -8,4 +8,8 @@ if [ -n "$PERSONAL_TF_API_KEY" ]; then
     cp /workspaces/fry-bot/.devcontainer/credentials.tfrc.json.example "$HOME/.terraform.d/credentials.tfrc.json"
     # shellcheck disable=SC2016
     sed -i -e 's/$TERRAFORM_API_KEY/'"$PERSONAL_TF_API_KEY"'/' "$HOME/.terraform.d/credentials.tfrc.json"
+
+    pushd /workspaces/fry-bot/infrastructure
+    terraform init
+    popd
 fi
