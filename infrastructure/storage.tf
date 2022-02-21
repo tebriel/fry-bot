f432edcfd9762139446d30d89578defbbdf5c174
Chris M <821688+tebriel@users.noreply.github.com>
2022-02-21 22:03:41 +0000

    Switch to cognitive search

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -16,11 +16,6 @@ resource "azurerm_storage_table" "haiku" {
   storage_account_name = azurerm_storage_account.fry-bot.name
 }
 
-resource "azurerm_storage_queue" "haiku" {
-  name                 = "haiku"
-  storage_account_name = azurerm_storage_account.fry-bot.name
-}
-
 resource "azurerm_role_assignment" "table-contributor" {
   for_each = toset([
     azurerm_user_assigned_identity.fry-bot.principal_id,
@@ -29,16 +24,5 @@ resource "azurerm_role_assignment" "table-contributor" {
 
   scope                = azurerm_storage_account.fry-bot.id
   role_definition_name = "Storage Table Data Contributor"
-  principal_id         = each.key
-}
-
-resource "azurerm_role_assignment" "queue-contributor" {
-  for_each = toset([
-    azurerm_user_assigned_identity.fry-bot.principal_id,
-    var.tebriel-id,
-  ])
-
-  scope                = azurerm_storage_account.fry-bot.id
-  role_definition_name = "Storage Queue Data Contributor"
   principal_id         = each.key
 }
