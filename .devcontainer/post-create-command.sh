17fecbf13e0be0b4178a264b5513e1b220a29816
Chris M <821688+tebriel@users.noreply.github.com>
2022-02-21 17:30:59 +0000

    Easier terraforming

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ -n "$PERSONAL_TF_API_KEY" ]; then
+    echo "Configuring terraform creds..."
+    mkdir -p "$HOME/.terraform.d/"
+    cp /workspaces/fry-bot/.devcontainer/credentials.tfrc.json.example "$HOME/.terraform.d/credentials.tfrc.json"
+    # shellcheck disable=SC2016
+    sed -i -e 's/$TERRAFORM_API_KEY/'"$PERSONAL_TF_API_KEY"'/' "$HOME/.terraform.d/credentials.tfrc.json"
+fi
