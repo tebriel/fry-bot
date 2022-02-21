48eec89e4ba2512867a0a862e08f4368eabb2ebc
Chris M <821688+tebriel@users.noreply.github.com>
2022-02-21 04:05:27 +0000

    Use the secret instead of storing everywhere

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM python:3.10
 
-ENV BOT_GATEWAY_TOKEN= AZURE_STORAGE_CONNECTION_STRING=
-
 WORKDIR /workdir
 COPY requirements.txt /workdir/requirements.txt
 RUN pip install -r requirements.txt
--- script/historical_wordles.py
+++ script/historical_wordles.py
@@ -3,12 +3,17 @@ import os
 import asyncio
 import hikari
 from bot import wordle
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
+credential = DefaultAzureCredential()
+secret_client = SecretClient('https://fry-bot.vault.azure.net/', credential)
 
 async def main():
     """history of channel."""
     rest_app = hikari.RESTApp()
-    async with rest_app.acquire(os.getenv('BOT_GATEWAY_TOKEN'), "Bot") as client:
+    secret = secret_client.get_secret('bot-gateway-token')
+    async with rest_app.acquire(secret.value, "Bot") as client:
         print('a')
         msgs = await client.fetch_messages(channel='901164244375056384', after='929052029458984970')
         print('b')
