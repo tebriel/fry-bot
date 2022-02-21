9faa58ee35b9d05000e7f7e3e1bf8a087566b544
Chris M <821688+tebriel@users.noreply.github.com>
2022-02-21 03:30:55 +0000

    Use client_id

--- infrastructure/key-vault.tf
+++ infrastructure/key-vault.tf
@@ -13,7 +13,7 @@ resource "azurerm_key_vault" "fry-bot" {
 resource "azurerm_key_vault_access_policy" "example" {
   key_vault_id = azurerm_key_vault.fry-bot.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_user_assigned_identity.fry-bot.id
+  object_id    = azurerm_user_assigned_identity.fry-bot.client_id
 
   secret_permissions = [
     "Get",
