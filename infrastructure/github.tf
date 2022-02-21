131cee18145d3dd8c63cf3efdf1a23d3d554ed4f
Chris M <821688+tebriel@users.noreply.github.com>
2022-02-21 15:50:59 +0000

    Clean up and try again

--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -1,9 +1,3 @@
-resource "github_actions_secret" "registry-url" {
-  repository      = "fry-bot"
-  secret_name     = "REGISTRY_URL"
-  plaintext_value = "gchr.io"
-}
-
 resource "github_actions_secret" "tf-api-token" {
   repository      = "fry-bot"
   secret_name     = "TF_API_TOKEN"
