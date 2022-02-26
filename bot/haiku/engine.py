de550dbb8ab75cd48689eb776e226ce6a1d19697
Chris M <821688+tebriel@users.noreply.github.com>
2022-02-26 18:34:45 +0000

    Clean up

--- bot/haiku/engine.py
+++ bot/haiku/engine.py
@@ -32,18 +32,13 @@ class Haiku:
             return "line not found in store"
         return entity.line
 
-    @staticmethod
-    def store_haiku(lines: List[str], c_date: str = None) -> None:
-        """Store a haiku."""
-        entity = FormedHaiku(
-            created_at=c_date,
-            poem=lines,
-        )
-        entity.save()
-
     @classmethod
-    def make_haiku(cls, seed_entity: HaikuLine = None) -> str:
-        """Make a haiku."""
+    def make_haiku(cls, seed_entity: HaikuLine = None) -> FormedHaiku:
+        """
+        Make a haiku.
+
+        todo: Use HaikuLines instead of strings
+        """
         lines: List[str] = [
             cls.random_line(HaikuKey.FIVE),
             cls.random_line(HaikuKey.SEVEN),
@@ -54,13 +49,17 @@ class Haiku:
                 lines[random.choice([0, 2])] = seed_entity.line
             else:
                 lines[1] = seed_entity.line
-        cls.store_haiku(lines)
-        return "\n".join(lines)
+
+        entity = FormedHaiku(poem=lines)
+        entity.save()
+        return entity
 
     @classmethod
     def about(cls, term: str = None) -> str:
         """Make a haiku about a term."""
         searches = HaikuSearch.search(term=term)
+        if not searches:
+            return f"I don't know about {term}"
 
         entity = random.choice(searches)
         return cls.make_haiku(seed_entity=entity)
