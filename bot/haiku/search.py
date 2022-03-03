d547445c96757faabc69a3fc48f179828a9fee93
Chris M <821688+tebriel@users.noreply.github.com>
2022-03-03 21:37:37 +0000

    Limit search results let tests pass

--- bot/haiku/search.py
+++ bot/haiku/search.py
@@ -6,33 +6,23 @@ from bot.haiku.models import HaikuKey, HaikuLine
 class HaikuSearch:
     """Finds HaikuLines from the database."""
 
-    @property
-    @classmethod
-    def clients(cls) -> dict[HaikuKey, SearchClient]:
+    @staticmethod
+    def clients() -> dict[HaikuKey, SearchClient]:
         """Initializes search clients."""
-        return getattr(cls, "__clients", {})
-
-    @clients.setter
-    @classmethod
-    def clients(cls, clients: dict[HaikuKey, SearchClient]):
-        """Initializes search clients."""
-        return setattr(cls, "__clients", clients)
+        return {
+            HaikuKey.FIVE: search_connect("haiku-fives-index"),
+            HaikuKey.SEVEN: search_connect("haiku-sevens-index"),
+        }
 
     @classmethod
     def search(cls, term: str, size: HaikuKey = None) -> list[HaikuLine]:
         """Search for lines."""
-        search_clients = cls.clients.values()
+        search_clients = cls.clients().values()
         if size is not None:
-            search_clients = cls.clients[size]
+            search_clients = cls.clients()[size]
 
         results = []
-        for result_set in [x.search(term) for x in search_clients]:
+        for result_set in [x.search(term, top=5) for x in search_clients]:
             for result in result_set:
                 results.append(HaikuLine.from_storage_table(result))
         return results
-
-
-HaikuSearch.clients = {
-    HaikuKey.FIVE: search_connect("haiku-fives-index"),
-    HaikuKey.SEVEN: search_connect("haiku-sevens-index"),
-}
