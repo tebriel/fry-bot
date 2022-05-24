7636c47b20ee7697efdf64cb5bf6642a630b2800
Chris M <821688+tebriel@users.noreply.github.com>
2022-05-24 15:56:41 +0000

    remove actions stuff

--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -3,9 +3,3 @@ resource "github_actions_secret" "tf-api-token" {
   secret_name     = "TF_API_TOKEN"
   plaintext_value = var.tf-api-token
 }
-
-resource "github_actions_secret" "workspace-key" {
-  repository      = "fry-bot"
-  secret_name     = "WORKSPACE_KEY"
-  plaintext_value = azurerm_log_analytics_workspace.fry-bot.primary_shared_key
-}
