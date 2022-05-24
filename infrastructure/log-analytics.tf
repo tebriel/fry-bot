f7286d0593bfc64c89d617bc2526b88de64a0738
Chris M <821688+tebriel@users.noreply.github.com>
2022-05-24 16:16:53 +0000

    fix fmt

--- infrastructure/log-analytics.tf
+++ infrastructure/log-analytics.tf
@@ -1,8 +1,8 @@
 resource "azurerm_log_analytics_workspace" "fry-bot" {
-  name                       = "fry-bot"
-  location                   = azurerm_resource_group.fry-bot.location
-  resource_group_name        = azurerm_resource_group.fry-bot.name
-  sku                        = "PerGB2018"
-  retention_in_days          = 30
-  daily_quota_gb             = 0.5
+  name                = "fry-bot"
+  location            = azurerm_resource_group.fry-bot.location
+  resource_group_name = azurerm_resource_group.fry-bot.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  daily_quota_gb      = 0.5
 }
