c7913029d677dfa6b02c2765f6259f5b00657c96
Chris M <821688+tebriel@users.noreply.github.com>
2022-05-24 16:10:23 +0000

    Wrong file

--- infrastructure/log-analytics.tf
+++ infrastructure/log-analytics.tf
@@ -6,9 +6,3 @@ resource "azurerm_log_analytics_workspace" "fry-bot" {
   retention_in_days          = 30
   daily_quota_gb             = 0.5
 }
-
-resource "github_actions_secret" "workspace-key" {
-  repository      = "fry-bot"
-  secret_name     = "WORKSPACE_KEY"
-  plaintext_value = azurerm_log_analytics_workspace.fry-bot.primary_shared_key
-}
