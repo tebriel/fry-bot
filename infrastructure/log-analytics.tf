5e41ac6b7336ea67bfe7a6c3d793042e7c066312
Chris M <821688+tebriel@users.noreply.github.com>
2022-05-24 14:44:15 +0000

    Set retention to 30d

--- infrastructure/log-analytics.tf
+++ infrastructure/log-analytics.tf
@@ -3,7 +3,7 @@ resource "azurerm_log_analytics_workspace" "fry-bot" {
   location                   = azurerm_resource_group.fry-bot.location
   resource_group_name        = azurerm_resource_group.fry-bot.name
   sku                        = "PerGB2018"
-  retention_in_days          = 7
+  retention_in_days          = 30
   daily_quota_gb             = 0.5
   internet_ingestion_enabled = false
   internet_query_enabled     = false
